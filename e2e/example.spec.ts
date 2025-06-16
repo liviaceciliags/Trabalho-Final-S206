fb3ac2640579924fc4eeb50491d848caefbbd84b
Livia <liviaceciliags@outlook.com>
2025-06-16 18:10:44 -0300

    Adicionando comentários corrigidos

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,15 +1,18 @@
+// Importa as funções do pacote do Playwright para definir testes e fazer asserts
 import { test, expect } from '@playwright/test';
 
-test('acessando página do Playwright', async ({page}) => {
-    // ações
+// Define um novo teste 
+test('Acessando página do Playwright', async ({ page }) => {
+
+    // Navega até a página inicial do Playwright
     await page.goto('https://playwright.dev');
 
-    // Localizador
+    // Define um localizador para o elemento que contém 'Get started'
     const botaoGetStarted = page.getByText('Get started');
 
-    // ações
+    // Executa ação de clique no botão localizado
     await botaoGetStarted.click();
-    
-    // verificações
+
+    // Verifica se a URL corresponde à página de introdução 
     await expect(page).toHaveURL('https://playwright.dev/docs/intro');
-});
+}); 
